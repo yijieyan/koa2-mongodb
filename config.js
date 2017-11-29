002cdf96960d55c80cb99a926e4ffceab46cc6a3
yijie.yan <yijie.yan@outlook.com>
2017-11-29 09:36:55 +0800

    增加支持多核cpu的模式

--- config.js
+++ config.js
@@ -4,23 +4,25 @@ global.env = process.env.environment || 'localhost';
 module.exports = {
     'development': {
         port: 5000,
-        dbUrl: 'mongodb://localhost:27017/test1',
+        dbUrl: 'mongodb://localhost:27017/vote',
         redis: {
             port: 6379,
             host: 'localhost'
-        }
+        },
+        enable: true   //是否开启利用多核cpu
     },
     'test': {
         port: 5000,
-        dbUrl: 'mongodb://localhost:27017/test1',
+        dbUrl: 'mongodb://localhost:27017/vote',
         redis: {
             port: 6379,
             host: 'localhost'
-        }
+        },
+        enable: true   //是否开启利用多核cpu
     },
     'prouction': {
         port: 5000,
-        dbUrl: 'mongodb://localhost:27017/test1',
+        dbUrl: 'mongodb://localhost:27017/vote',
         redis: {
             port: 6379,
             host: 'localhost'
@@ -28,10 +30,11 @@ module.exports = {
     },
     'localhost': {
         port: 5000,
-        dbUrl: 'mongodb://localhost:27017/test1',
+        dbUrl: 'mongodb://localhost:27017/vote',
         redis: {
             port: 6379,
             host: 'localhost'
-        }
+        },
+        enable: true   //是否开启利用多核cpu
     }
 };
