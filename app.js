dd02dc90e70ab45072c3740fb177b9025b34674c
yijie.yan <yijie.yan@outlook.com>
2018-03-06 18:53:27 +0800

    mongoose 升级5.x

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const json = require('koa-json');
 const bodyparser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const config = require('./config')[env];
-const db = require('./libs/db');
+require('./libs/db');
 const error = require('./middleware/error');
 const auth = require('./middleware/auth');
-
 
 const public1 = require('./routes/public');
 const users = require('./routes/users');
@@ -16,14 +15,12 @@ process.env.dataDir = __dirname;
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }));
 app.use(json());
 app.use(logger());
 app.use(error);
 app.use(require('koa-static')(__dirname + '/public'));
-
-
 
 // logger
 app.use(async (ctx, next) => {
@@ -37,7 +34,6 @@ app.use(auth);
 // routes
 app.use(public1.routes(), public1.allowedMethods());
 app.use(users.routes(), users.allowedMethods());
-
 
 app.listen(`${config.port}`);
 console.log(`listen ${config.port} ...`);
--- libs/db.js
+++ libs/db.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 const config = require('../config')[env];
 
 mongoose.Promise = global.Promise;
-let connection = mongoose.connect(config.dbUrl, {config: {autoIndex: false}, useMongoClient: true});
+    let count = 0;
+    let db;
+    mongoose.set('debug', true);
+    connect();
+    function connect() {
+      mongoose.connect(config.dbUrl);
+      db = mongoose.connection;
+      db.on('error', (err) => {
+         console.log('mongoose err :',err);
+      });
+      db.once('open', () => {
+        console.log(`connect ${config.dbUrl} successful!`);
+      });
+      db.on('disconnected', () => {
+        if (count < 3) {
+          connect();
+          count++;
+        }else {
+          throw new Error('数据库挂了,快去修吧。');
+        }
+      })
+    }
 
-module.exports = connection;
+module.exports = db;
